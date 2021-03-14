c31206fca5fed7e655a1134f9df01396dc26d6ab
Sandro Hanea <sandro@shane.ro>
2021-03-14 12:43:28 +0100

    Improved the usability.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 2021
+To celebrate Pi Day, I created this nice but useless tool to deploy an ASCII Art to github contribution history.
+
+# Usage
+git clone
+cd src
+dotnet run <yourFileName> <someGithubRepository>
+enjoy your ascii art work on github
+
+<yourFileName> should be a text file with the next connotation:
+for each char, a day will be filled with commits in girhub
+(space) : no commits
+'x' : 3 commits(light green)
+'X' : 9 commits(dark green) 
+
+Use maximum 52 chars width and 7 chars height.
+
+<someGithubRepository> in order to deploy to another git account, use github PAT when providing the url to the repository.
---
+++ src/WorkspaceDirectory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace AsciiArtDeployer
+{
+    public sealed class WorkspaceDirectory : IDisposable
+    {
+        private readonly DirectoryInfo directoryInfo;
+
+        public WorkspaceDirectory()
+        {
+            Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Pi AsciiArtDeployer", Guid.NewGuid().ToString());
+            directoryInfo = Directory.CreateDirectory(Path);
+        }
+
+        public string Path { get; set; }
+
+        public string GetFilePath(string fileName)
+        {
+            return System.IO.Path.Combine(Path, fileName);
+        }
+
+        public void Dispose()
+        {
+            SetAttributesNormal(directoryInfo);
+            directoryInfo.Delete(recursive: true);
+        }
+
+        private void SetAttributesNormal(DirectoryInfo directoryInfo)
+        {
+            foreach (var subdirectory in directoryInfo.GetDirectories())
+            {
+                SetAttributesNormal(subdirectory);
+            }
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                file.Attributes = FileAttributes.Normal;
+            }
+        }
+    }
+}
